fdba37125b57563a25fcfb582b4723b69cac569f
Logan Ballard <ballardl@por-ballardl-m.ds.ad.adp.com>
2019-09-26 16:28:18 -0700

    addingin initial logging'

--- src/server/main/app.js
+++ src/server/main/app.js
@@ -1,13 +1,17 @@
 'use strict';
 const express = require('express');
 const json_parser = require('body-parser').json();
+const morgan = require('morgan');
 const acct_router = require('./routes/account_route');
 const action_router = require('./routes/action_route');
 const app = express();
 const port = 3000;
+const log_level = (process.env.NODE_ENV === 'test' ) ? false : (process.env.NODE_ENV === 'dev') ? 'dev' : 'tiny';
+
 let router = express.Router();
 
 app.use(json_parser);
+if (log_level) app.use(morgan(log_level));
 app.use('/', router);
 
 router.use('/account', acct_router);
